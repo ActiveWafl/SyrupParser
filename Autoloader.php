5b85300d56310917de592ba3dca7ffeb103a037f
Evan <Evan@192.168.1.104>
2014-05-04 00:47:47 -0700

    moved/reorganizing

---
+++ Autoloader.php
@@ -0,0 +1,11 @@
+<?php
+spl_autoload_register(function($classname){
+	if (substr($classname,0,10)=="Wafl\\Syrup" || substr($classname,0,11)=="\\Wafl\\Syrup")
+	{
+		$filePath = __DIR__.DIRECTORY_SEPARATOR.str_replace("\\", DIRECTORY_SEPARATOR, $classname).".php";
+		if (file_exists($filePath))
+		{
+			require_once ($filePath);
+		}
+	}
+});?>
